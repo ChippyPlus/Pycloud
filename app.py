7ce3b93917beccc3ccb76873c52cba3f5f325feb
Adam Mathlay <mathlayadam@gmail.com>
2024-06-23 00:07:20 +0200

    added basicArithmetic endpoints to app.py to be registered

--- app.py
+++ app.py
@@ -13,14 +13,17 @@ import appRoutes.math.eva
 
 app = Flask("pi-cloud!")
 
-app.register_blueprint(appRoutes.math.add.math_index_bp)
-
+app.register_blueprint(appRoutes.math.add.bp)
+app.register_blueprint(appRoutes.math.sub.bp)
+app.register_blueprint(appRoutes.math.mul.bp)
+app.register_blueprint(appRoutes.math.div.bp)
+app.register_blueprint(appRoutes.math.pow.bp)
+app.register_blueprint(appRoutes.math.mod.bp)
+app.register_blueprint(appRoutes.math.eva.bp)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
-@app.route('/hello')
-def hello():
-    return 'Hello World2!'
+
 
